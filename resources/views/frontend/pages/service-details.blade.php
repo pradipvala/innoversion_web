e5582260c28587cb84cab27b57a0fac446b75c2b
Mohit Gohel <54216595+MohitGohel07@users.noreply.github.com>
2026-04-15 11:01:44 +0530

    Enhance service details: update UI/UX Design and SEO sections with improved descriptions, hero titles, and features for better clarity and engagement; update service image handling in the service details page for fallback support

--- resources/views/frontend/pages/service-details.blade.php
+++ resources/views/frontend/pages/service-details.blade.php
@@ -25,8 +25,8 @@
             <div class="hero-container">
                 <div class="d-flex flex-column gspace-5">
                     <div class="image-container">
-                        <img src="{{ asset('image/' . $service['image']) }}" alt="{{ $service['name'] }} Banner"
-                            class="single-service-img" style="object-fit: cover;">
+                        <img src="{{ asset('image/' . ($service['image'] ?? 'tech-banner.png')) }}"
+                            alt="{{ $service['name'] }} Banner" class="single-service-img" style="object-fit: cover;">
                         <div class="single-service-title-layout">
                             <div class="">
                                 <div class="single-service-spacer"></div>
